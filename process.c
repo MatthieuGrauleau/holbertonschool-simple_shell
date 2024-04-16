8138948b24ba3033c03f98a67cdc7107b11f58a6
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-16 16:19:03 +0200

    adding condition

--- process.c
+++ process.c
@@ -3,6 +3,7 @@
 void process(char *line)
 {
 	pid_t pid;
+	int status;
 	char *args[2];
 
 	args[0] = line;
@@ -15,6 +16,18 @@ void process(char *line)
 		if (execve(line, args, NULL) == -1)
 		{
 			perror("./hsh");
+			exit(EXIT_FAILURE);
+		}
+	} else if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("waitpid");
 			exit(EXIT_FAILURE);
 		}
 	}
