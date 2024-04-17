2fb0d6f954fcf43a5ae14361010806f96fdffc21
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-17 12:24:08 +0200

    merging

--- process.c
+++ process.c
@@ -5,10 +5,17 @@ void process(char *line)
 	pid_t pid;
 	int status;
 	
-	char *args[2];
+	char *token;
+	char *args[64];
 	int i = 0;
 
-	line[strcspn(line, "\n")] = 0;
+	token = strtok(line, " \t\n");
+
+	while (token != NULL)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \t\n");
+	}
 
 	args[i] = NULL;
 
@@ -16,9 +23,7 @@ void process(char *line)
 
 	if (pid == 0)
 	{
-		args[0] = line;
-		args[1] = NULL;
-		if (execve(line, args, NULL) == -1)
+		if (execve(args[0], args, NULL) == -1)
 		{
 			perror("./hsh");
 			exit(EXIT_FAILURE);
@@ -36,6 +41,5 @@ void process(char *line)
 			perror("waitpid");
 			exit(EXIT_FAILURE);
 		}
-		
 	}
 }
