9f00d64c142c77b47e2e6f9640d6fa37560e6335
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-19 09:22:27 +0200

    changing tokens for args

--- str_tok.c
+++ str_tok.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * str_tok - function that split the line from get line in tokens
+ * str_tok - function that split the line from get line in args
  * @line: array of char from get line
- * Return: the tokens
+ * Return: the args
 */
 char **str_tok(char *line)
 {
 	char *token;
-	char **tokens;
+	char **args;
 	int size = 64;
 	int i = 0;
 
-	tokens = malloc(size * sizeof(char *));
-	if (tokens == NULL)
+	args = malloc(size * sizeof(char *));
+	if (args == NULL)
 	{
 		fprintf(stderr, "problem allocation memory for token \n");
 		exit(EXIT_FAILURE);
@@ -23,21 +23,21 @@ char **str_tok(char *line)
 
 	while (token != NULL)
 	{
-		tokens[i] = token;
+		args[i] = token;
 		i++;
 		if (i >= size)
 		{
 			size += size;
-			tokens = realloc(tokens, size * sizeof(char *));
-			if (tokens == NULL)
+			args = realloc(args, size * sizeof(char *));
+			if (args == NULL)
 			{
-				fprintf(stderr, "problem allocation memory for tokens \n");
+				fprintf(stderr, "problem allocation memory for args \n");
 				exit(EXIT_FAILURE);
 			}
 		}
 		token = strtok(NULL, " \t\n");
 	}
 
-	tokens[i] = NULL;
-	return (tokens);
+	args[i] = NULL;
+	return (args);
 }
