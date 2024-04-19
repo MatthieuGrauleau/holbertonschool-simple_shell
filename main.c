7dadeed39fd9b32379e14a762f0761e38472b4d3
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-19 11:49:35 +0200

    main.c

--- main.c
+++ main.c
@@ -1,40 +1,39 @@
-#include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "main.h"
 
-#define BUFFER_SIZE 1024
+/**
+ * main - entry point
+ *
+ * Return: 0 for success.
+*/
 
-int main(void) {
-    char *buffer = NULL;
-    size_t bufsize = 0;
-    char *args[2];
-    int status;
+int main(void)
+{
+	char *line;
+	char **tokens;
 
-    while (1) {
-        printf("$ ");
-        fflush(stdout);
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		while (1)
+		{
+			prompt();
+			line = get_line();
+			tokens = str_tok(line);
+			process(tokens);
+			free(tokens);
+			free(line);
+		}
 
-        if (getline(&buffer, &bufsize, stdin) == -1) {
-            break;
-        }
-
-        buffer[strcspn(buffer, "\n")] = '\0';
-
-        args[0] = buffer;
-        args[1] = NULL;
-
-        if (fork() == 0) {
-            if (execve(args[0], args, NULL) == -1) {
-                perror("./hsh");
-            }
-            exit(EXIT_FAILURE);
-        } else {
-            wait(&status);
-        }
-    }
-
-    free(buffer);
-    return 0;
+	}
+	else
+	{
+		while (1)
+		{
+			line = get_line();
+			tokens = str_tok(line);
+			process(tokens);
+			free(tokens);
+			free(line);
+		}
+	}
+	return (0);
 }
