ffa4afe320b303a0c0467212f85540c411f9d6e8
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-19 14:21:46 +0200

    trying to implement a do while instead of a while

--- main.c
+++ main.c
@@ -10,30 +10,39 @@ int main(void)
 {
 	char *line;
 	char **tokens;
+	int status = 0;
 
 	if (isatty(STDIN_FILENO) == 1)
 	{
-		while (1)
+		do
 		{
 			prompt();
 			line = get_line();
 			tokens = str_tok(line);
-			process(tokens);
+			status = process(tokens);
+			if (status == -1)
+			{
+				exit(status);
+			}
 			free(tokens);
 			free(line);
-		}
+		}  while (status == 0);
 
 	}
 	else
 	{
-		while (1)
+		do
 		{
 			line = get_line();
 			tokens = str_tok(line);
-			process(tokens);
+			status = process(tokens);
+			if (status == -1)
+			{
+				exit(status);
+			}
 			free(tokens);
 			free(line);
-		}
+		}  while (status == 0);
 	}
 	return (0);
 }
