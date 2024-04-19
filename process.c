249af751f62df9e102ff3424ee7f1d70111a8360
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-19 16:37:51 +0200

    changing return status in process

--- process.c
+++ process.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <sys/wait.h>
+#include <errno.h>
 
 /**
  * process - function that process.
@@ -6,27 +8,49 @@
  * @token: pointer to an array of string from str_tok.
  * Return: 0;
 */
+
+
 int process(char **token)
 {
-	pid_t pid;
+    pid_t pid;
+    int status;
 
-	pid = fork();
+    pid = fork();
 
-	if (pid == 0)
-	{
-		if (execve(token[0], token, NULL) == -1)
-		{
-			perror("./hsh");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-	{
-		perror("fork");
-	}
-	else
-	{
-		waitpid(pid, NULL, 0);
-	}
-	return (-1);
+    if (pid == -1)
+    {
+        perror("fork");
+        return -1;
+    }
+    else if (pid == 0)
+    {
+        if (execve(token[0], token, NULL) == -1)
+        {
+            perror("./hsh");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        if (waitpid(pid, &status, 0) == -1)
+        {
+            perror("waitpid");
+            return -1;
+        }
+
+        if (WIFEXITED(status))
+        {
+            int exit_status = WEXITSTATUS(status);
+            if (exit_status != 0)
+            {
+                printf("Child process exited with status %d\n", exit_status);
+            }
+        }
+        else if (WIFSIGNALED(status))
+        {
+            int signal_number = WTERMSIG(status);
+            printf("Child process terminated by signal %d\n", signal_number);
+        }
+    }
+    return 0;
 }
