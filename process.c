75da99964669b4102c58239c80110477544b9d7b
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-22 13:34:12 +0200

    creating the path

--- process.c
+++ process.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define PATH_MAX 4096
+
 /**
  * process - function that process.
  *
@@ -11,20 +13,17 @@
 int process(char **token, char **av, int path)
 {
 	pid_t pid;
-	int status, built;
+	int status;
 	char *phraze = "%s: %d: %s: not found\n";
-
-
+	char *path_env = getenv("PATH");
 
 	if (token[0] == NULL)
 	{
 		return (1);
 	}
-
-	built = built_in(token);
-	if (built != 0)
+	if ((strcmp(token[0], "env") == 0))
 	{
-		return (built);
+		env(environ);
 	}
 
 	pid = fork();
@@ -33,6 +32,24 @@ int process(char **token, char **av, int path)
 	{
 		if (execve(token[0], token, environ) == -1)
 		{
+			if (path_env != NULL)
+			{
+				char *paths = strtok(path_env, ":");
+				while (paths != NULL)
+				{
+					char full_path[PATH_MAX];
+					snprintf(full_path, sizeof(full_path), "%s/%s", paths, token[0]);
+					if (access(full_path, X_OK) == 0)
+					{
+						if (execv(full_path, token) == -1)
+						{
+							perror("execv");
+						}
+					}
+					paths =strtok(NULL, ":");
+				}
+			}
+			
 			fprintf(stderr, phraze, av[0], path, token[0]);
 			free(token);
 			exit(errno);
