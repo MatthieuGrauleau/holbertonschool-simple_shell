a90ae6f49132dd82239cb529d15d459bd8453a6c
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-23 11:24:39 +0200

    handling fork

--- process.c
+++ process.c
@@ -25,7 +25,6 @@ int process(char **token, char **av, int path)
     {
         return (built);
     }
-
     while (*env != NULL)
     {
         if (strncmp(*env, "PATH=", 5) == 0)
@@ -36,47 +35,48 @@ int process(char **token, char **av, int path)
         env++;
     }
 
-    pid = fork();
-
-    if (pid == 0)
+    if (paths != NULL)
     {
-        if (execve(token[0], token, environ) == -1)
+        paths = strtok(paths, ":");
+        while (paths != NULL)
         {
-            if (paths != NULL)
+            size_t full_path_len = strlen(paths) + 1 + strlen(token[0]) + 1;
+            char *full_path = malloc(full_path_len);
+            if (full_path == NULL)
             {
-                paths = strtok(paths, ":");
-                while (paths != NULL)
+                perror("malloc");
+                exit(1);
+            }
+            strcpy(full_path, paths);
+            strcat(full_path, "/");
+            strcat(full_path, token[0]);
+            if (access(full_path, X_OK) == 0)
+            {
+                pid = fork();
+                if (pid == 0)
                 {
-                    size_t full_path_len = strlen(paths) + 1 + strlen(token[0]) + 1;
-                    char *full_path = malloc(full_path_len);
-                    if (full_path == NULL)
+                    if (execve(full_path, token, environ) == -1)
                     {
                         exit(1);
                     }
-                    strcpy(full_path, paths);
-                    strcat(full_path, "/");
-                    strcat(full_path, token[0]);
-                    if (access(full_path, X_OK) == 0)
-                    {
-                        if (execve(full_path, token, environ) == -1)
-                        {
-                            exit(1);
-                        }
-                    }
-                    paths = strtok(NULL, ":");
-                    free(full_path);
+                }
+                else if (pid < 0)
+                {
+                    perror("fork");
+                    exit(1);
+                }
+                else
+                {
+                    wait(&status);
+                    return (status);
                 }
             }
-
-            fprintf(stderr, phraze, av[0], path, token[0]);
-            free(token);
-            exit(1);
+            free(full_path);
+            paths = strtok(NULL, ":");
         }
     }
-    else
-    {
-        wait(&status);
-        return (status);
-    }
-    return (0);
+
+    fprintf(stderr, phraze, av[0], path, token[0]);
+    free(token);
+    return (1);
 }
