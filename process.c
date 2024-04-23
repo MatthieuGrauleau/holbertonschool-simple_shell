ab7849295ceda1860ade4c7947af86cc082e3608
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-23 13:35:07 +0200

    push to change

--- process.c
+++ process.c
@@ -8,13 +8,12 @@
  * @path: ID of program to print in stdou
  * Return: 0;
  */
-int process(char **token, char **av, int path)
+int process(char **token, char **av, int path, int c)
 {
     pid_t pid;
     int status, built;
     char *phraze = "%s: %d: %s: not found\n";
-    char **env = environ;
-    char *paths = NULL;
+    
 
     if (token[0] == NULL)
     {
@@ -25,15 +24,7 @@ int process(char **token, char **av, int path)
     {
         return (built);
     }
-    while (*env != NULL)
-    {
-        if (strncmp(*env, "PATH=", 5) == 0)
-        {
-            paths = *env + 5;
-            break;
-        }
-        env++;
-    }
+
     pid = fork();
 
     if (pid == 0)
@@ -41,34 +32,9 @@ int process(char **token, char **av, int path)
     {
         if (execve(token[0], token, environ) == -1)
         {
-            if (paths != NULL)
-            {
-                paths = strtok(paths, ":");
-                while (paths != NULL)
-                {
-                    char *full_path = malloc(strlen(paths) + 1 + strlen(token[0]) + 1);
-                    if (full_path == NULL)
-                    {
-						free(full_path);
-                        exit(1);
-                    }
-                    strcpy(full_path, paths);
-                    strcat(full_path, "/");
-                    strcat(full_path, token[0]);
-                    if (access(full_path, X_OK) == 0)
-                    {
-                        if (execve(full_path, token, environ) == -1)
-                        {
-							free(full_path);
-                            exit(1);
-                        }
-                    }
-                    paths = strtok(NULL, ":");
-                    free(full_path);
-                }
-            }
-
             fprintf(stderr, phraze, av[0], path, token[0]);
+            if (!c)
+				free(token[0]);
             free(token);
             exit(1);
         }
