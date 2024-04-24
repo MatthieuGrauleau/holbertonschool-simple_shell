9b80bbb97ac41a776e4a37a051fcb357d0880af5
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-24 14:32:59 +0200

    creation of a new function handle_path

--- process.c
+++ process.c
@@ -3,9 +3,9 @@
 /**
  * process - function that process.
  *
- * @token: pointer to an array of string from str_tok.
- * @av: name of prorgam to print in stdou
- * @path: ID of program to print in stdou
+ * @token: Pointer to an array of strings from str_tok.
+ * @av: Name of program to print in stdout.
+ * @path: ID of program to print in stdout.
  * Return: 0;
  */
 int process(char **token, char **av, int path)
@@ -13,64 +13,27 @@ int process(char **token, char **av, int path)
     pid_t pid;
     int status, built;
     char *phraze = "%s: %d: %s: not found\n";
-    char **env = environ;
-    char *paths = NULL;
 
-    if (token[0] == NULL)
-    {
+    if (!token || !token[0])
         return (0);
-    }
-    
+
     built = built_in(token);
     if (built != 0)
-    {
         return (built);
-    }
 
-    while (*env != NULL)
-    {
-        if (strncmp(*env, "PATH=", 5) == 0)
-        {
-            paths = *env + 5;
-            break;
-        }
-        env++;
-    }
     pid = fork();
+    if (pid == -1)
+        return (1);
+
     if (pid == 0)
     {
         if (execve(token[0], token, environ) == -1)
         {
-            if (paths != NULL)
+            if (handle_path(token, phraze, av, path) == 1)
             {
-                paths = strtok(paths, ":");
-                while (paths != NULL)
-                {
-                    char *full_path = malloc(strlen(paths) + 1 + strlen(token[0]) + 1);
-                    if (full_path == NULL)
-                    {
-						free(full_path);
-                        exit(1);
-                    }
-                    strcpy(full_path, paths);
-                    strcat(full_path, "/");
-                    strcat(full_path, token[0]);
-                    if (access(full_path, X_OK) == 0)
-                    {
-                        if (execve(full_path, token, environ) == -1)
-                        {
-							free(full_path);
-                            exit(1);
-                        }
-                    }
-                    paths = strtok(NULL, ":");
-                    free(full_path);
-                }
+                free(token);
+                exit(127);
             }
-
-            fprintf(stderr, phraze, av[0], path, token[0]);
-            free(token);
-            exit(127);
         }
     }
     else
