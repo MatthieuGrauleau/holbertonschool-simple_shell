2058a56d67e31bbac5055f099d3e4081894a9135
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-25 08:36:53 +0200

    adding comments

--- process.c
+++ process.c
@@ -17,18 +17,19 @@ int process(char **token, char **av, int path)
 	if (!token || !token[0])
 		return (0);
 
-	built = built_in(token);
+	built = built_in(token); /*Check if the command is a built-in function*/
 	if (built != 0)
 		return (built);
 
-	pid = fork();
+	pid = fork(); /*Fork a child process*/
 	if (pid == -1)
-		return (1);
+		return (1); /*Return error if fork fails*/
 
-	if (pid == 0)
+	if (pid == 0) /*Child process*/
 	{
-		if (execve(token[0], token, environ) == -1)
+		if (execve(token[0], token, environ) == -1) /*Execute the command*/
 		{
+			/*If command not found, try to find in PATH*/
 			if (handle_path(token, phraze, av, path) == 1)
 			{
 				free(token);
@@ -36,9 +37,9 @@ int process(char **token, char **av, int path)
 			}
 		}
 	}
-	else
+	else /* Parent process*/
 	{
-		wait(&status);
+		wait(&status); /*Wait for the child process to complete*/
 		return (status);
 	}
 	return (0);
