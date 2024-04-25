f4c750f9b420f7f8a40f7b35d8d7c570d6802de0
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-25 08:34:11 +0200

    adding comments

--- str_tok.c
+++ str_tok.c
@@ -21,11 +21,11 @@ char **str_tok(char *line)
 	if (args == NULL)
 		return (NULL);
 
-	token = strtok(line, " \r\t\n");
+	token = strtok(line, " \r\t\n"); /*Tokenize the input line*/
 
 	while (token != NULL)
 	{
-		args[i] = token;
+		args[i] = token; /*Store each token in the args array*/
 		i++;
 		if (i >= size)
 		{
@@ -34,9 +34,9 @@ char **str_tok(char *line)
 			if (args == NULL)
 				return (NULL);
 		}
-		token = strtok(NULL, " \t\r\n");
+		token = strtok(NULL, " \t\r\n"); /*Get the next token*/
 	}
 
 	args[i] = NULL;
-	return (args);
+	return (args); /*Return the arguments array*/
 }
